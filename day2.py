3211eb5eabbc47ad1211c6762ab8b7b253c0109c
Amanda Guan <121919044+amandaguan-ag@users.noreply.github.com>
2024-01-12 10:47:43 -0800

    Update day2.py

--- day2.py
+++ day2.py
@@ -1,3 +1,5 @@
+# Create a program to calculate the area of a circle given its radius.
+
 import math
 
 def area_of_circle(r):
@@ -5,9 +7,9 @@ def area_of_circle(r):
 
 while True:
   try:
-    r=input("Enter radius:")
+    r=float(input("Enter radius:"))
     a=area_of_circle(r)
     print(f"Area of circle with radisu {r} is approximate to {a}")
     break
-  except valueError:
+  except ValueError:
     print("Please enter a only numbers for the radius.")
