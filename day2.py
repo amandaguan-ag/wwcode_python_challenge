6a1c579efc1cab168dac47d4cb7e06edea749fed
Amanda Guan <ag.amandaguan@gmail.com>
2024-01-12 15:34:16 -0800

    Add period for printed sentence

--- day2.py
+++ day2.py
@@ -9,7 +9,7 @@ while True:
   try:
     r=float(input("Enter radius:"))
     a=area_of_circle(r)
-    print(f"Area of circle with radisu {r} is approximate to {a}")
+    print(f"Area of circle with radisu {r} is approximate to {a}.")
     break
   except ValueError:
     print("Please enter a only numbers for the radius.")
