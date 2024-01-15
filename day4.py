fd5d46a1ed6b7c8eb7c563139d09db6d4e304d7f
Amanda Guan <ag.amandaguan@gmail.com>
2024-01-15 13:19:07 -0800

    use sum method

--- day4.py
+++ day4.py
@@ -1,16 +1,10 @@
 # Write a program to find the sum of all elements in a list.
 
-def sum_of_list(list):
-  sum=0
-  for num in list:
-    sum+=num
-  return sum
-
 while True:
   try:
     num_str=input("Enter numbers separated by spaces: ")
     list=list(map(int, num_str.split()))
-    result=sum_of_list(list)
+    result=sum(list)
     print(f"The sum of the {list} is: {result}.")
     break
   except ValueError:
