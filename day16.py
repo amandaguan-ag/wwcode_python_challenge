2ef37afd2275875282291c91f8798af500d54dd3
Amanda Guan <ag.amandaguan@gmail.com>
2024-01-26 23:21:21 -0800

    remove unecessary import

--- day16.py
+++ day16.py
@@ -1,11 +1,6 @@
 # Write a function that counts the frequency of each word in a str.
-from collections import Counter
-import string
 
 def frequency(str):
-  translator = str.maketrans("", "", string.punctuation)
-  cleaned_str = str.translate(translator)
-
   freq_dict = {}
   for char in str:
       if char in '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~' or char==" ":
